870b96503eebe3a3c680cc6aa185e538cdb41f6b
mauraaninditakirana <kiranamaura351@gmail.com>
2024-06-03 14:35:37 +0700

    membuat cout untuk membuka dan membaca file, kemudian menambahkan if infile open jika filenya ada, dan while dalam if untuk melakukan perulangan pada tiap barisnya dan ditampilkan

--- iOFile.cpp
+++ iOFile.cpp
@@ -29,4 +29,17 @@ int main() {
     ifstream infile;
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        
+    }
 }
