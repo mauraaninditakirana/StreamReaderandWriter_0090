8bd0cbc33c17157776745c9e05fd80eecbad5365
mauraaninditakirana <kiranamaura351@gmail.com>
2024-06-03 14:28:01 +0700

    menambahkan cout untuk mendapatkan setiap karakter dalam suatu baris, dan getline supaya loop berhenti jika memasukkan karakter q, dan menambhakan if untukmenulis dan memasukkan nilai dari baris ke file

--- iOFile.cpp
+++ iOFile.cpp
@@ -14,6 +14,12 @@ int main() {
 
     //unlimited loop untuk menulis
     while (true) {
-       
+        cout << "- ";
+        //mendapatkan  setiap karakter dalam suatu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris =="q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file 
+        outfile << baris << endl;
     }
 }
